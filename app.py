38365e106cc4e9028ef0b73e78e40fa3b8e0ebcf
Harsh Raj Mishra <harshrajmishra.hrm@gmail.com>
2024-12-21 18:51:59 +0530

    print

--- app.py
+++ app.py
@@ -18,3 +18,4 @@ def fetch_data(search, country, language, selected_options, time_interval):
     print(f"Keyword: {search}")
     print(f"Selected Options: {selected_options}")
     print(f"Country: {country}")
+    print
