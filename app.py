0f3aaf5640be958e5b7c141feea43792188ec97c
Harsh Raj Mishra <harshrajmishra.hrm@gmail.com>
2025-01-10 01:00:39 +0530

    query

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def fetch_data(search, country, language, selected_options, time_interval):
                         
                         # Prepare the data to be stored in the CSV file
                         data = [search, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
-                        for i in range(min(25, len(related_queries))):                    
+                        for i in range(min(25, len(related_queries))):
+                            query = related_queries.iloc[i]['query']                    
