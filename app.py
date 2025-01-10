46af4927caf3a7c74358918e05f3949f404dbbfc
Harsh Raj Mishra <harshrajmishra.hrm@gmail.com>
2025-01-11 02:03:29 +0530

    s

--- app.py
+++ app.py
@@ -79,4 +79,5 @@ def fetch_data(search, country, language, selected_options, time_interval):
                             
                         # Define the CSV file path
                         csv_file_path = "related_queries_data.csv"
+                        
                         # Check if the CSV file already exists                        
