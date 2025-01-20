51c91e5ea2db22bafaab80112e0d43701c1d1285
Harsh Raj Mishra <harshrajmishra.hrm@gmail.com>
2025-01-20 22:02:48 +0530

    comment

--- app.py
+++ app.py
@@ -118,4 +118,6 @@ def fetch_data(search, country, language, selected_options, time_interval):
                                 data.extend([topic, percentage])
                                 
                             # Define the CSV file path
-                            csv_file_path = "related_topics_data.csv"                                       
+                            csv_file_path = "related_topics_data.csv"
+                            
+                            # Check if the CSV file already exists                                       
