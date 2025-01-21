d788a38c428e383f0cd6a954558ee2a77faf91c6
Harsh Raj Mishra <harshrajmishra.hrm@gmail.com>
2025-01-22 00:27:45 +0530

    for

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def fetch_data(search, country, language, selected_options, time_interval):
                                 
                                 # Write the header row if the file doesn't exist
                                 if not file_exists:
-                                    header = ['Keyword', 'Date']                                      
+                                    header = ['Keyword', 'Date']
+                                    for i in range(1, min(26, len(related_topics) + 1)):                                      
