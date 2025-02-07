ed68cca4c11be1d89e4927bb7d35c29f152788ab
Harsh Raj Mishra <harshrajmishra.hrm@gmail.com>
2025-02-07 12:56:22 +0530

    str

--- app.py
+++ app.py
@@ -174,4 +174,5 @@ def fetch_data(search, country, language, selected_options, time_interval):
                     for i in range(7):
                         day_start = start_date + datetime.timedelta(days=i)
                         day_end = day_start + datetime.timedelta(days=1)
-                        day_start_str = day_start.strftime('%Y-%m-%d')                                                          
+                        day_start_str = day_start.strftime('%Y-%m-%d')
+                        day_end_str = day_end.strftime('%Y-%m-%d')                                                          
